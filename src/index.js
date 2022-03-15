aa576c31b76c27eed4b6c9d4db832bcb1ce6249c
lironoshka111 <46004238+lironoshka111@users.noreply.github.com>
2022-03-16 00:14:43 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { useCallback, useMemo, useState } from "react";
+import ReactDOM from "react-dom";
+import Todos from "./Todo";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = useCallback(() => {
+    setTodos((t) => [...t, "New Todo"]);
+  }, []);
+
+  return (
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
